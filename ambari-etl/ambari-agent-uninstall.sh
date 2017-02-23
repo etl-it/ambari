7aa7acf136fb6ae8fc220190c8ea4dd220bf789f
Ryu_S3ki <rbe.jz.gn@gmail.com>
2017-02-23 14:57:24 +0100

    Mount
    Añadiendo mount rw ro para editar /usr

--- ambari-etl/ambari-agent-uninstall.sh
+++ ambari-etl/ambari-agent-uninstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mount -o remount,rw /usr
+
 ambari-agent stop
 ambari-agent reset
 rm -rf /var/lib/ambari-agent
@@ -8,3 +10,5 @@ rm -rf /usr/lib/ambari-agent
 rm -rf /usr/lib/python2.6/site-packages/ambari-agent
 
 dpkg --purge ambari-agent
+
+mount -o remount,ro /usr
